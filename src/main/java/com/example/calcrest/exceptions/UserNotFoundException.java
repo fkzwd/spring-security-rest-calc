6b06e3b90bee0cd1c9fcbe53b3adc9b94e6b8a1d
Airat_Salikhov <airatchaplin@gmail.com>
2021-02-11 12:36:34 +0400

    init commit

--- src/main/java/com/example/calcrest/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.calcrest.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
